de364ac4a0b63d0c244a87c928e1b1e49ed2eee0
unknown <basimaslam1234@gmail.com>
2023-03-13 03:07:40 -0700

    djf

---
+++ script.js
@@ -0,0 +1,72 @@
+// var student={
+//     name:"adil",
+//     age:10,
+//     place:"thrissur"
+// };
+// console.log(student);
+// console.log(student.age);
+
+// var cars={
+//     name:"polo",
+//     model:"GT",
+//     color:"black",
+//     price:976000
+// };
+// console.log(cars);
+// console.log(cars.model);
+// var fruits=["apple","orange","banana",10];
+// console.log(fruits[0]);
+// console.log(fruits[3]);
+// console.log(fruits.length);
+// fruits.push(11);
+// fruits.unshift("grapes");
+// fruits.shift(1);
+
+// console.log(fruits);
+
+// var person=[
+
+//     {
+//         name:'adil',
+//         age:45,
+//         place:'gh'
+//     },
+//     {
+//         name:"lida",
+//         age:22,
+//         place:'hq'
+//     }
+// ];
+// console.log(person[1].name);
+// function add(a,b){
+//     var c=a+b;
+//     console.log(c);
+    
+// }
+// add(1,4);
+// function mul(a,b){
+//     var c=a*b;
+//    console.log(c);
+    
+// }
+// // mul(1,2);
+// const dsn =(a,b)=>{
+//     let c=a/b;
+//     console.log(c);
+// }
+// dsn(4,2)
+// var x=20;
+// if(x>20){
+//     console.log('good'); 
+// }
+// else{
+//     console.log('bad');
+// }
+var fruits=["apple","orange","banana",'GRAPES','mango'];
+// for(i=0;i<fruits.length;i++)
+// console.log(fruits[i]);
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++;
+    }while(i<fruits.length)
